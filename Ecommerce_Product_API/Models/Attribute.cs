93ae2da8248cd75e81f70643904d1270402815bf
joaomcarniel <joaomarcoscrr@gmail.com>
2026-07-16 16:35:35 +0100

    TEst

---
+++ Ecommerce_Product_API/Models/Attribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce_Product_API.Models;
+
+public partial class Attribute
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<AttributeValue> AttributeValues { get; set; } = new List<AttributeValue>();
+}
