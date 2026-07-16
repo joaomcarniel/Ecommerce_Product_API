5278d6d8851380a6822a9df7e0c294e552b19bfe
joaomcarniel <joaomarcoscrr@gmail.com>
2026-07-16 16:31:41 +0100

    Test

---
+++ Ecommerce_Product_API/Models/AttributeValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce_Product_API.Models;
+
+public partial class AttributeValue
+{
+    public int Id { get; set; }
+
+    public int AttributeId { get; set; }
+
+    public string AttributeValue1 { get; set; } = null!;
+
+    public virtual Attribute Attribute { get; set; } = null!;
+}
---
+++ Ecommerce_Product_API/Models/VariantAttributeValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce_Product_API.Models;
+
+public partial class VariantAttributeValue
+{
+    public int Id { get; set; }
+
+    public int VariantId { get; set; }
+
+    public int AttributeValueId { get; set; }
+
+    public virtual ProductVariant Variant { get; set; } = null!;
+}
