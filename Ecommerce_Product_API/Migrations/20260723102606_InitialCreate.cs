25932b8e0827a8086644108f0a51275f5787ac48
joaomcarniel <joaomarcoscrr@gmail.com>
2026-07-23 11:56:32 +0100

    Migration and gitignore

---
+++ Ecommerce_Product_API/Migrations/20260723102606_InitialCreate.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Ecommerce_Product_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "attributes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("attributes_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    product_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    short_description = table.Column<string>(type: "text", nullable: false),
+                    long_descriptionProduct = table.Column<string>(type: "text", nullable: true),
+                    brand_product = table.Column<string>(type: "text", nullable: true),
+                    category_product = table.Column<string>(type: "text", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true),
+                    create_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    update_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("product_pkey", x => x.product_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "attribute_values",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    attribute_id = table.Column<int>(type: "integer", nullable: false),
+                    attribute_value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("attribute_values_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "attribute_id_fkey",
+                        column: x => x.attribute_id,
+                        principalTable: "attributes",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_variants",
+                columns: table => new
+                {
+                    variant_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    product_id = table.Column<int>(type: "integer", nullable: false),
+                    sku = table.Column<string>(type: "text", nullable: false),
+                    base_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    sale_price = table.Column<decimal>(type: "numeric", nullable: false),
+                    stock = table.Column<int>(type: "integer", nullable: false),
+                    create_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    update_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("variant_pkey", x => x.variant_id);
+                    table.ForeignKey(
+                        name: "product_fkey",
+                        column: x => x.product_id,
+                        principalTable: "products",
+                        principalColumn: "product_id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "variant_attribute_values",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    variant_id = table.Column<int>(type: "integer", nullable: false),
+                    attribute_value_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("variant_attribute_values_pkey", x => x.id);
+                    table.ForeignKey(
+                        name: "variant_fkey",
+                        column: x => x.variant_id,
+                        principalTable: "product_variants",
+                        principalColumn: "variant_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_attribute_values_attribute_id",
+                table: "attribute_values",
+                column: "attribute_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_variants_product_id",
+                table: "product_variants",
+                column: "product_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_variant_attribute_values_variant_id",
+                table: "variant_attribute_values",
+                column: "variant_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "attribute_values");
+
+            migrationBuilder.DropTable(
+                name: "variant_attribute_values");
+
+            migrationBuilder.DropTable(
+                name: "attributes");
+
+            migrationBuilder.DropTable(
+                name: "product_variants");
+
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
